2ad7f9d2221143567bc84d30bfbebecada0e6734
apariitmadras <24ds1000011@ds.study.iitm.ac.in>
2025-08-05 02:26:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ async def analyze(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn, os
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
